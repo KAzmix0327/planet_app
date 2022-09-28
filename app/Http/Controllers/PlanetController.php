afbd1cb9feb95fb5bdd4f46c0a62fb3e03998a88
KAzumix0327 <yamauchi138@icloud.com>
2022-09-28 21:42:35 +0900

    惑星情報管理アプリCRUD機能作成

---
+++ app/Http/Controllers/PlanetController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\planet;
+use App\Http\Requests\PlanetRequest;
+
+class PlanetController extends Controller
+{
+    // indexページへ移動
+    public function index()
+    {
+        // モデル名::テーブル全件取得
+        $planets = Planet::all();
+        // memosティレクトリーの中のindexページを指定し、memosの連想配列を代入
+        return view('planets.index', ['planets' => $planets]);
+    }
+
+    // createページへ移動
+    public function create()
+    {
+        return view('planets.create');
+    }
+
+    // storeページへ移動
+    public function store(PlanetRequest $request)
+    {
+        // インスタンスの作成
+        $planet = new Planet;
+
+        // 値の用意
+        $planet->name = $request->name;
+        $planet->en_name = $request->en_name;
+        $planet->radius = $request->radius;
+        $planet->weight = $request->weight;
+
+        // インスタンスに値を設定して保存
+        $planet->save();
+
+        // 登録したらindexに戻る
+        return redirect('/planets');
+    }
+
+    // showページへ移動
+    public function show($id)
+    {
+        $planet = Planet::find($id);
+        return view('planets.show', ['planet' => $planet]);
+    }
+
+    // editページへ移動
+    public function edit($id) 
+    {
+        $planet = Planet::find($id);
+        return view('planets.edit', ['planet' => $planet]);
+    }
+
+    // updateページへ移動
+    public function update(planetRequest $request, $id) 
+    {
+        // ここはidで探して持ってくる以外はstoreと同じ
+        $planet = Planet::find($id);
+
+        // 値の用意
+        $planet->name = $request->name;
+        $planet->en_name = $request->en_name;
+        $planet->radius = $request->radius;
+        $planet->weight = $request->weight;
+
+        // 保存
+        $planet->save();
+
+        // 登録したらindexに戻る
+        return redirect('/planets');
+    }
+    
+    // destroyページへ移動
+        public function destroy($id)
+    {
+        $planet = Planet::find($id);
+        $planet->delete();
+        return redirect('/planets');
+    }
+}
+
---
+++ database/seeders/PlanetSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PlanetSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+// 一件だけinsertする
+        DB::table('planets')->insert([
+            'name' => '水星',
+            'en_name' => 'MERCURY',
+            'radius' => '2439',
+            'weight' => '3310',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+        # paramに配列を代入
+        $param = [
+            [
+                'name' => '金星',
+                'en_name' => 'VENUS',
+                'radius' => '6052',
+                'weight' => '48700',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+        # DB::table->insertでレコードの登録
+        DB::table('planets')->insert($param);
+    }
+}
